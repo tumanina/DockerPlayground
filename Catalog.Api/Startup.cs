57ffea1c8f76e2d5f4fecac25e5c86c048eb4e73
Olga Tumanina <olga.tumanina@jedlix.com>
2022-04-18 19:47:33 +0200

    Fixed swageer registration

--- Catalog.Api/Startup.cs
+++ Catalog.Api/Startup.cs
@@ -38,9 +38,10 @@ namespace Catalog.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog Api v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog Api v1"));
 
             app.UseHttpsRedirection();
 
